8aabbbcb52b823781327323334106f2cfe75d716
Unai <andres.161757@e.unavarra.es>
2026-05-20 18:51:07 +0200

    [green] - dado dos numeros devolver el ultimo numero

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -7,6 +7,10 @@ class StringCalculator
     public function add(string $numbers): int
     {
         if(empty($numbers)) return 0;
-        return (int) $numbers;
+
+        if(!str_contains($numbers, ',')) return (int) $numbers;
+
+        $valores = explode(",", $numbers);
+        return $valores[1];
     }
 }
