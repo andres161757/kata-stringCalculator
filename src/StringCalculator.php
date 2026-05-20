e4d250479eede9406e4d60a9a53d3998d3ad98e6
Unai <andres.161757@e.unavarra.es>
2026-05-20 19:16:55 +0200

    [green] - dado dos numeros con \n devolver suma de numeros

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -8,9 +8,12 @@ class StringCalculator
     {
         if(empty($numbers)) return 0;
 
-        if(!str_contains($numbers, ',')) return (int) $numbers;
+        if(!str_contains($numbers, ',') && !str_contains($numbers, "\n")) return (int) $numbers;
+        if(str_contains($numbers, "\n")) {
+            $numbers = str_replace("\n", ',', $numbers);
+        };
 
-        $valores = explode(",", $numbers);
+        $valores = explode(',', $numbers);
         $suma = 0;
 
         for ($i = 0; $i < count($valores); $i++) {
